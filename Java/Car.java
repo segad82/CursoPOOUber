41eb2698b3c10d38fa03ab22cbeeecab0b17311b
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-02-17 13:16:59 -0300

    Se agregan clase en PHP y JS, además se agrega ejemplo sobre instanciar objetos en Python y Java

--- Java/Car.java
+++ Java/Car.java
@@ -3,4 +3,9 @@ class Car {
     String license;
     String driver;
     Integer passengers;
+
+    public void printDataCar() {
+        System.out.println("License: " + this.license + "\nDriver: " + this.driver);
+    }
+
 }
--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,8 @@
+from car import Car
+
 if __name__ == "__main__":
     print('Hola Mundo')
+    car = Car()
+    car.license = ''
+    car.driver = ''
+    print(vars(car))
